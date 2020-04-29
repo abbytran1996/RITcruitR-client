89e21ff6ca4bc332dcb27970643101ffbff262bb
mt2915 <mt2915@g.rit.edu>
2020-04-29 03:52:10 -0700

    Changed environment link to server link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 
 export const environment = {
     mode: 'Production',
-    api_url: 'http://localhost:8181'
-    //api_url: 'http://3.234.39.206:8181/'
+    //api_url: 'http://localhost:8181'
+    api_url: 'http://3.234.39.206:8181/'
 }
