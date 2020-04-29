20c73ec0f50620eb666df3483da9f2767365b403
mt2915 <mt2915@g.rit.edu>
2020-04-28 18:29:20 -0700

    updated server links

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,6 @@
 export const environment = {
     mode: 'Development',
     api_url: 'http://localhost:8181',
-    //api_url: 'http://18.221.204.144:8080'
     //api_url: 'http://3.234.39.206:8181/'
 }
 // TODO: Remove the remote api_url eventually, just here to toggle between
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     mode: 'Production',
-    // api_url: 'http://35.170.211.62:8080'
     api_url: 'http://localhost:8181'
     //api_url: 'http://3.234.39.206:8181/'
 }
