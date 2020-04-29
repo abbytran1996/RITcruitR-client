081256d06fe8d0a382008cb0461081c3950bb636
pdfkpb <pdfkpb@gmail.com>
2020-04-29 18:14:14 -0400

    big ol' update

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,7 +4,7 @@
 export const environment = {
     mode: 'Development',
     //api_url: 'http://localhost:8181',
-    api_url: 'http://3.234.39.206:8181/'
+    api_url: 'http://3.234.39.206:8181'
 }
 // TODO: Remove the remote api_url eventually, just here to toggle between
 // remote and local server easily during dev without needing to enable
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 export const environment = {
     mode: 'Production',
     //api_url: 'http://localhost:8181'
-    api_url: 'http://3.234.39.206:8181/'
+    api_url: 'http://3.234.39.206:8181'
 }
